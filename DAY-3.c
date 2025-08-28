3fa27539f2600601eea819b7acae30b0449011fa
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-08-29 01:25:09 +0530

    DAY-3.c

--- DAY-3.c
+++ DAY-3.c
@@ -1,32 +1,35 @@
 "Q5-Write a program to convert temperature from Celsius to Fahrenheit"
 
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main() {
     double celsius, fahrenheit;
-    cout << "Enter temperature in Celsius: ";
-    cin >> celsius;
+    printf("Enter temperature in Celsius: ");
+    scanf("%lf", &celsius);
+
     fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
-    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    printf("Temperature in Fahrenheit: %.2f\n", fahrenheit);
     return 0;
 }
+
 
 "Q6-Write a program to swap two numbers using a third variable"
 
     
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main() {
     int a, b, temp;
-    cout << "Enter two numbers: ";
-    cin >> a >> b;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
     temp = a;
     a = b;
     b = temp;
 
-    cout << "After swapping:\n";
-    cout << "a = " << a << "\nb = " << b << endl;
+    printf("After swapping:\n");
+    printf("a = %d\nb = %d\n", a, b);
     return 0;
 }
+
