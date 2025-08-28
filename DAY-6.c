05e4c71768eb98c6708d372a618493e63c08d1c2
Shivam Tyagi <shivam.tyagi23123@gmail.com>
2025-08-29 01:28:55 +0530

    DAY-6.c

--- DAY-6.c
+++ DAY-6.c
@@ -1,17 +1,16 @@
 "Q11-Write a program to input an integer and check whether it is even or odd using if–else"
 
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main() {
     int num;
-    cout << "Enter an integer: ";
-    cin >> num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
     if (num % 2 == 0) {
-        cout << num << " is even." << endl;
+        printf("%d is even.\n", num);
     } else {
-        cout << num << " is odd." << endl;
+        printf("%d is odd.\n", num);
     }
 
     return 0;
@@ -20,22 +19,21 @@ int main() {
 
 "Q12-Write a program to input an integer and check whether it is positive, negative or zero using nested if–else."
 
-#include <iostream>
-using namespace std;
+#include <cstdio>
 
 int main() {
     int num;
-    cout << "Enter an integer: ";
-    cin >> num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
 
     if (num >= 0) {
         if (num == 0) {
-            cout << "The number is zero." << endl;
+            printf("The number is zero.\n");
         } else {
-            cout << "The number is positive." << endl;
+            printf("The number is positive.\n");
         }
     } else {
-        cout << "The number is negative." << endl;
+        printf("The number is negative.\n");
     }
 
     return 0;
